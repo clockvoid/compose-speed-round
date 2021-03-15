b9e636b93625e457760494e43611b0a4a9db8a70
clockvoid <hidenari.t1997nov@gmail.com>
2021-03-16 02:14:58 +0900

    add: components which is used for all screens

--- app/src/main/java/com/example/androiddevchallenge/ui/component/PrimaryButton.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/component/PrimaryButton.kt
@@ -1,4 +1,46 @@
 package com.example.androiddevchallenge.ui.component
 
-class PrimaryButton {
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun PrimaryButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        shape = MaterialTheme.shapes.medium,
+        colors = buttonColors(
+            backgroundColor = MaterialTheme.colors.secondary,
+            contentColor = MaterialTheme.colors.onSecondary
+        ),
+        modifier = Modifier
+            .height(48.dp)
+            .fillMaxWidth()
+    ) {
+        Text(text = text, style = MaterialTheme.typography.button)
+    }
+}
+
+@Preview("Light Theme")
+@Composable
+fun LightPrimaryButtonPreview() {
+    MyTheme {
+        PrimaryButton(text = "push me", onClick = {})
+    }
+}
+
+@Preview("Dark Theme")
+@Composable
+fun DarkPrimaryButtonPreview() {
+    MyTheme(darkTheme = true) {
+        PrimaryButton(text = "push me", onClick = {})
+    }
 }
--- app/src/main/java/com/example/androiddevchallenge/ui/component/PrimaryTextField.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/component/PrimaryTextField.kt
@@ -1,2 +1,61 @@
 package com.example.androiddevchallenge.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun PrimaryTextField(
+    state: MutableState<String>,
+    modifier: Modifier = Modifier,
+    label: @Composable () -> Unit
+) {
+    OutlinedTextField(
+        value = state.value,
+        onValueChange = { value -> state.value = value },
+        label = label,
+        textStyle = MaterialTheme.typography.body1,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    )
+}
+
+@Preview("Light Theme")
+@Composable
+fun LightPrimaryTextFieldPreview() {
+    val state = mutableStateOf("")
+
+    MyTheme {
+        Box(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
+            PrimaryTextField(state = state, modifier = Modifier.fillMaxWidth()) {
+                Text("hello")
+            }
+        }
+    }
+}
+
+@Preview("Dark Theme")
+@Composable
+fun DarkPrimaryTextFieldPreview() {
+    val state = mutableStateOf("")
+
+    MyTheme(darkTheme = true) {
+        Box(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
+            PrimaryTextField(state = state, modifier = Modifier.fillMaxWidth()) {
+                Text("hello")
+            }
+        }
+    }
+}
