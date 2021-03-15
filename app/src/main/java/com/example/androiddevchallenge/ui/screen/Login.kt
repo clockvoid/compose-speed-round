1ad0374f25f97c2d0ff5c11f8c0e126e1d378bb4
clockvoid <hidenari.t1997nov@gmail.com>
2021-03-16 02:43:52 +0900

    tweak: login screen

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/Login.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/Login.kt
@@ -3,8 +3,8 @@ package com.example.androiddevchallenge.ui.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,6 +13,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -29,7 +30,7 @@ fun Login(navController: NavController) {
     Surface(color = MaterialTheme.colors.background) {
         Column(
             modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp, top = 184.dp)
+                .padding(start = 16.dp, end = 16.dp)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -37,28 +38,44 @@ fun Login(navController: NavController) {
                 text = "Log in with email",
                 color = MaterialTheme.colors.onBackground,
                 modifier = Modifier
+                    .paddingFromBaseline(top = 184.dp)
                     .padding(bottom = 16.dp),
                 style = MaterialTheme.typography.h1
             )
+
             PrimaryTextField(state = state, label = {
                 Text(text = "Email address", style = MaterialTheme.typography.body2)
             }, modifier = Modifier.padding(bottom = 8.dp))
+
             PrimaryTextField(state = state, label = {
                 Text(text = "Password (8+ characters)", style = MaterialTheme.typography.body2)
-            }
-            )
-            Row(modifier = Modifier.height(24.dp), verticalAlignment = Alignment.Bottom) {
+            })
+
+            Row(modifier = Modifier.paddingFromBaseline(top = 24.dp)) {
                 Text(
                     text = "By clicking below, you agree to our ",
                     style = MaterialTheme.typography.body2
                 )
-                Text(text = "Terms of Use", style = MaterialTheme.typography.body2)
+
+                Text(
+                    text = "Terms of Use",
+                    style = MaterialTheme.typography.body2,
+                    textDecoration = TextDecoration.Underline
+                )
+
                 Text(text = " and consent", style = MaterialTheme.typography.body2)
             }
-            Row(modifier = Modifier.padding(bottom = 16.dp)) {
+
+            Row(modifier = Modifier.paddingFromBaseline(bottom = 16.dp)) {
                 Text(text = "to our ", style = MaterialTheme.typography.body2)
-                Text(text = "Privacy Policy", style = MaterialTheme.typography.body2)
+
+                Text(
+                    text = "Privacy Policy",
+                    style = MaterialTheme.typography.body2,
+                    textDecoration = TextDecoration.Underline
+                )
             }
+
             PrimaryButton(text = "Log in", onClick = { navController.navigate("home") })
         }
     }
