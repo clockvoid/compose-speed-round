1a906a337dd69f869e09d3a7fa3c2059d9512ae4
clockvoid <hidenari.t1997nov@gmail.com>
2021-03-16 02:20:02 +0900

    add: pseudo data pool

--- app/src/main/java/com/example/androiddevchallenge/data/GardenListData.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/GardenListData.kt
@@ -1,2 +1,31 @@
 package com.example.androiddevchallenge.data
 
+import android.net.Uri
+import com.example.androiddevchallenge.model.Garden
+
+val gardenListData = listOf(
+    Garden(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/3097770/pexels-photo-3097770.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Monstera"
+    ),
+    Garden(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/4751978/pexels-photo-4751978.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Aglaonema"
+    ),
+    Garden(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/4425201/pexels-photo-4425201.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Peace lily"
+    ),
+    Garden(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/6208087/pexels-photo-6208087.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Fiddle leaf tree"
+    ),
+    Garden(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/2123482/pexels-photo-2123482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Snake planet"
+    ),
+    Garden(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/1084199/pexels-photo-1084199.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Pathos"
+    )
+)
--- app/src/main/java/com/example/androiddevchallenge/data/GardenThemeData.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/GardenThemeData.kt
@@ -1,4 +1,27 @@
 package com.example.androiddevchallenge.data
 
-class GardenThemeData {
-}
+import android.net.Uri
+import com.example.androiddevchallenge.model.GardenTheme
+
+val gardenThemeData = listOf(
+    GardenTheme(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/2132227/pexels-photo-2132227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Desert chic"
+    ),
+    GardenTheme(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/1400375/pexels-photo-1400375.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Tiny terrariums"
+    ),
+    GardenTheme(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/5699665/pexels-photo-5699665.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Jungle vibes"
+    ),
+    GardenTheme(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/6208086/pexels-photo-6208086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Easy care"
+    ),
+    GardenTheme(
+        imageUrl = Uri.parse("https://images.pexels.com/photos/3511755/pexels-photo-3511755.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
+        name = "Statements"
+    ),
+)
