cb3e990f6a99045bc3feede82dcd7b06c87bc9ae
clockvoid <hidenari.t1997nov@gmail.com>
2021-03-16 02:17:36 +0900

    move: login screen to screen package

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/Login.kt
@@ -0,0 +1,78 @@
+package com.example.androiddevchallenge.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.ui.component.PrimaryButton
+import com.example.androiddevchallenge.ui.component.PrimaryTextField
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun Login() {
+    val state = remember { mutableStateOf("") }
+
+    Surface(color = MaterialTheme.colors.background) {
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 184.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Log in with email",
+                color = MaterialTheme.colors.onBackground,
+                modifier = Modifier
+                    .padding(bottom = 16.dp),
+                style = MaterialTheme.typography.h1
+            )
+            PrimaryTextField(state = state, label = {
+                Text(text = "Email address", style = MaterialTheme.typography.body2)
+            }, modifier = Modifier.padding(bottom = 8.dp))
+            PrimaryTextField(state = state, label = {
+                Text(text = "Password (8+ characters)", style = MaterialTheme.typography.body2)
+            }
+            )
+            Row(modifier = Modifier.height(24.dp), verticalAlignment = Alignment.Bottom) {
+                Text(
+                    text = "By clicking below, you agree to our ",
+                    style = MaterialTheme.typography.body2
+                )
+                Text(text = "Terms of Use", style = MaterialTheme.typography.body2)
+                Text(text = " and consent", style = MaterialTheme.typography.body2)
+            }
+            Row(modifier = Modifier.padding(bottom = 16.dp)) {
+                Text(text = "to our ", style = MaterialTheme.typography.body2)
+                Text(text = "Privacy Policy", style = MaterialTheme.typography.body2)
+            }
+            PrimaryButton(text = "Log in", onClick = {})
+        }
+    }
+}
+
+@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun LightLoginPreview() {
+    MyTheme {
+        Login()
+    }
+}
+
+@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Composable
+fun DarkLoginPreview() {
+    MyTheme(darkTheme = true) {
+        Login()
+    }
+}
